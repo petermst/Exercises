96192effbbdc30ff463e871b05e7c4dfbe3c05b7
petermst <stige_94@hotmail.com>
2017-01-12 15:09:20 +0100

    Oppgave 4 i c

--- ex1_4.c
+++ ex1_4.c
@@ -1,1 +1,31 @@
 
+#include <pthread.h>
+#include <stdio.h>
+
+static int i = 0;
+
+void* someThreadFunction1(){
+	for(int j = 0; j < 1000000; j++){
+		i++;
+	}
+	return NULL;
+}
+
+void* someThreadFunction2(){
+	for(int k = 0; k < 1000000; k++){
+		i--;
+	}
+	return NULL;
+}
+
+int main(void){
+	pthread_t someThread1;
+	pthread_create(&someThread1, NULL, someThreadFunction1, NULL);
+	pthread_t someThread2;
+	pthread_create(&someThread2, NULL, someThreadFunction2, NULL);
+	pthread_join(someThread1, NULL);
+	pthread_join(someThread2, NULL);
+	printf("Dette er tallet: %d\n", i);
+	return 0;
+}
+
