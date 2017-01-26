26c8f1190cb5c908ac6b9eab5003b0cd2fe2006d
petermst <stige_94@hotmail.com>
2017-01-26 13:54:51 +0100

    Working UDP unit

--- Client.go
+++ Client.go
@@ -1,1 +1,48 @@
-package Client
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
+
+func main() {
+	buffer := make([]byte, 1024)
+	ListenerAddress := ":20008"
+	SenderAddress := "129.241.187.43:20008"
+
+	address, _ := net.ResolveUDPAddr("udp", ListenerAddress)
+	listener, _ := net.ListenUDP("udp", address)
+
+	addr, _ := net.ResolveUDPAddr("udp", SenderAddress)
+	conn, err := net.DialUDP("udp", nil, addr)
+
+	fmt.Printf("%s\n", listener.LocalAddr())
+	fmt.Printf("%s\n", conn.RemoteAddr())
+
+	checkError(err)
+	//sendMSG := make([]byte, 1024)
+
+	for {
+
+		time.Sleep(time.Second)
+
+		_, err := conn.Write([]byte("HEIEH\x00"))
+
+		checkError(err)
+
+		n, addr, _ := listener.ReadFromUDP(buffer)
+
+		fmt.Println(string(buffer[0:n]))
+		fmt.Printf("%s\n", addr)
+	}
+	os.Exit(0)
+}
